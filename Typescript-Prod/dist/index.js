d445ba12b3b2933c236c5c3560323c68dd93f3f1
lokeshdarla <lokeshnagasaidarla@gmail.com>
2024-02-11 18:30:56 +0530

    feat: Day 2 of diving into TS[classes, getters, setters]

--- Typescript-Prod/dist/index.js
+++ Typescript-Prod/dist/index.js
@@ -1,2 +1,34 @@
 "use strict";
-console.log("This is tyepscript file");
+// class User {
+//   public email: string;
+//   private name: string;
+//   // #name:string Private variables also declared like this
+//   readonly city: string ="CITY" // Default Values and readonly only to read not to write
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+class User {
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+        this.city = "CITY"; // Default Values and readonly only to read not to write
+        this._courseCount = 1;
+    }
+    get getGoogleEmail() {
+        return `Google${this.email}`;
+    }
+    get CourseCount() {
+        return this._courseCount;
+    }
+    //Setters cannot be a void functions
+    set couserCount(courseNum) {
+        if (courseNum <= 1)
+            throw new Error("Course count should be more than 1");
+        this._courseCount = courseNum;
+    }
+}
+const Lokesh = new User("Lokesh@.com", "Lokesh");
+//Lokesh.name 
+//Property 'name' is private and only accessible within class 'User'
--- Typescript-Prod/src/index.ts
+++ Typescript-Prod/src/index.ts
@@ -1,1 +1,46 @@
-console.log("This is tyepscript file")
+// class User {
+//   public email: string;
+//   private name: string;
+//   // #name:string Private variables also declared like this
+//   readonly city: string ="CITY" // Default Values and readonly only to read not to write
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+
+
+class User {
+  readonly city: string ="CITY" // Default Values and readonly only to read not to write
+
+  private _courseCount:number =1
+  
+  constructor(
+      public email: string,
+      private name: string) {
+  }
+
+  get getGoogleEmail() : string
+  {
+    return `Google${this.email}`
+  }
+
+  get CourseCount () :number{
+    return this._courseCount
+  }
+
+  //Setters cannot be a void functions
+
+  set couserCount (courseNum:number){
+    if(courseNum<=1) 
+    throw new Error("Course count should be more than 1")
+    this._courseCount=courseNum
+  }
+
+
+}
+
+const Lokesh = new User("Lokesh@.com", "Lokesh");
+
+//Lokesh.name 
+//Property 'name' is private and only accessible within class 'User'
